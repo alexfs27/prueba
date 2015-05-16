0a2eac489530f980ae45b4810c7467e5454bd37c
Joel Fajardo Salcedo <alexfs.1824@gmail.com>
2015-05-16 03:12:57 +0000

    prueba

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: blogs
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  about      :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Blog < ActiveRecord::Base
+    
+    validates :title, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  
+  resources :blogs do #/blogs/l/posts
   resources :posts
-  resources :blogs
+end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
